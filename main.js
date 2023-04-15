0e461d270e1db5c262b3633e9f6b068a0ac99592
victor-code <vdcloayza@gmail.com>
2023-04-15 18:59:28 -0400

    colors effects added

--- main.js
+++ main.js
@@ -1,19 +1,45 @@
 window.onload = () => {
-  let corazon = document.getElementById("corazoncito");
-  let fondo = document.querySelector(".fondo");
-  let estrellita = document.querySelector("#estrellita");
+  let corazon = document.getElementById(
+    "corazoncito"
+  );
+  let fondoCorazon = document.querySelector(
+    ".fondoCorazon"
+  );
+  let estrellita = document.querySelector(
+    "#estrellita"
+  );
+  const formulario = document.querySelector(
+    ".formularioContainer"
+  );
+  const cerrarIcon = document.querySelector(
+    "#cerrar_icon_8"
+  );
+  const buttonsContainer = document.querySelector(
+    ".buttonsContainer"
+  );
 
-  const formulario = document.querySelector(".formularioContainer");
+  cerrarIcon.addEventListener("mousedown", () => {
+    cerrarBehavior();
+  });
+  function cerrarBehavior() {
+    formulario.style.visibility = "hidden";
+    buttonsContainer.style.visibility = "visible";
+    cerrarIcon.setAttribute("width", "200");
+  }
+
   estrellita.addEventListener("click", () => {
     console.log(formulario.style.visibility);
 
     if (
       !formulario.style.visibility ||
-      formulario.style.visibility == "visible"
+      formulario.style.visibility == "hidden"
     ) {
-      formulario.style.visibility = "hidden";
-    } else {
       formulario.style.visibility = "visible";
+      buttonsContainer.style.visibility =
+        "hidden";
+      formulario.style.gridArea = "1 / 2 / 6 / 5";
+    } else {
+      formulario.style.visibility = "hidden";
     }
   });
 
@@ -21,14 +47,30 @@ window.onload = () => {
     changeColor();
   });
   function changeColor() {
-    if (!fondo.style.fill) {
-      fondo.style.fill = "#e1338b";
-      fondo.style.animation = "disolver 1 1s";
-      console.log(fondo.style.animationName + " transition added");
+    console.log(
+      "1° test: " + !fondoCorazon.style.fill
+    );
+    if (!fondoCorazon.style.fill) {
+      fondoCorazon.style.fill = "#e1338b";
+      fondoCorazon.style.animation =
+        "disolver 1 1.2s";
+      console.log(
+        fondoCorazon.style.animationName +
+          " transition added"
+      );
     } else {
-      fondo.style.animationName = "none";
-      fondo.style.fill = "";
-      console.log(fondo.style.animationName);
+      fondoCorazon.style.animationName = "none";
+      fondoCorazon.style.fill = "";
+      console.log(
+        fondoCorazon.style.animationName
+      );
     }
   }
+};
+const changeToYellow = () => {
+  const bodyImage =
+    document.querySelector(".container");
+  bodyImage.style.transition = "all 3s";
+  bodyImage.style.filter =
+    "hue-rotate(99deg) saturate(300%) contrast(90%) brightness(190%)";
 };
